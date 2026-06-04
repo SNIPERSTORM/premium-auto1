0663f708d62a5a63eb57dec3a605b07725a2e018
SNIPERSTORM <sedov_ns@mail.ru>
2026-06-04 04:11:47 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,20 @@
 const CACHE_NAME = 'premium-auto-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/cars.html',
-  '/feedback.html',
-  '/contacts.html',
-  '/styles.css'
+  '/premium-auto1/',
+  '/premium-auto1/index.html',
+  '/premium-auto1/cars.html',
+  '/premium-auto1/feedback.html',
+  '/premium-auto1/contacts.html',
+  '/premium-auto1/styles.css',
+  '/premium-auto1/images/logo.png',
+  '/premium-auto1/images/banner.jpg',
+  '/premium-auto1/images/car1.jpg',
+  '/premium-auto1/images/car2.jpg',
+  '/premium-auto1/images/car3.jpg',
+  '/premium-auto1/images/car4.jpg',
+  '/premium-auto1/images/car5.jpg',
+  '/premium-auto1/images/car6.jpg',
+  '/premium-auto1/images/showroom.jpg'
 ];
 
 self.addEventListener('install', event => {
