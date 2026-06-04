b9ae912f1c4381365d191c8e3486dd13cd6a519f
SNIPERSTORM <sedov_ns@mail.ru>
2026-06-04 04:18:28 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,34 +5,37 @@ const urlsToCache = [
   '/premium-auto1/cars.html',
   '/premium-auto1/feedback.html',
   '/premium-auto1/contacts.html',
-  '/premium-auto1/styles.css',
-  '/premium-auto1/images/logo.png',
-  '/premium-auto1/images/banner.jpg',
-  '/premium-auto1/images/car1.jpg',
-  '/premium-auto1/images/car2.jpg',
-  '/premium-auto1/images/car3.jpg',
-  '/premium-auto1/images/car4.jpg',
-  '/premium-auto1/images/car5.jpg',
-  '/premium-auto1/images/car6.jpg',
-  '/premium-auto1/images/showroom.jpg'
+  '/premium-auto1/styles.css'
 ];
 
 self.addEventListener('install', event => {
+  console.log('SW: Установка');
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('SW: Кэширование файлов');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.log('SW: Ошибка кэширования', err))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
 
 self.addEventListener('activate', event => {
+  console.log('SW: Активация');
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
